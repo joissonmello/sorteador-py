bb2b8b2748a5bfbe8015b44b9e17eba015fc5ea9
Joisson Mello <joisson.mello@softfocus.com.br>
2022-03-02 19:01:04 -0300

    ranking

--- sorteadorcredilab/sorteador/sorteio/api/views.py
+++ sorteadorcredilab/sorteador/sorteio/api/views.py
@@ -3,6 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
 from rest_framework import status, filters
 from rest_framework.response import Response
+from django.db.models import Count
 
 from sorteadorcredilab.sorteador.sorteio.api.serializers import SorteioSerializer
 from sorteadorcredilab.sorteador.models import Sorteio
@@ -24,4 +25,35 @@ class SorteioView(ModelViewSet):
         sorteios = Sorteio.objects.filter(data__gt=data_inicio, data__lt=data_fim)
 
         return Response(SorteioSerializer(sorteios, many=True).data, status=status.HTTP_200_OK)
+
+    @action(methods=['get'], detail=False, url_path='ranking')
+    def ranking(self, request):
+        ano = request.query_params.get('ano')
+
+        sorteios = Sorteio.objects.all()
+
+        if ano:
+            sorteios = Sorteio.objects.filter(data__year=ano)
+
+        facilitadores = list(
+            sorteios
+            .values('facilitador', 'facilitador__nome', 'facilitador__avatar')
+            .annotate(total=Count('id'))
+            .order_by('-total')
+            .values('facilitador', 'facilitador__nome', 'facilitador__avatar', 'total')
+        )
+
+        secretarios = list(
+            sorteios
+            .values('secretario', 'secretario__nome', 'secretario__avatar')
+            .annotate(total=Count('id'))
+            .order_by('-total')
+            .values('secretario', 'secretario__nome', 'secretario__avatar', 'total')
+        )
+
+        return Response({
+            'total_sorteios': sorteios.count(),
+            'facilitadores': facilitadores,
+            'secretarios': secretarios,
+        }, status=status.HTTP_200_OK)
 
